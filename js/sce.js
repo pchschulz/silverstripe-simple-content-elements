e380f0ae1d18ffacba9b269cbe17583213fade4e
Benedikt Hofstätter <hofstaetter.benni@gmail.com>
2017-12-08 15:06:17 +0100

    update

--- js/sce.js
+++ js/sce.js
@@ -1,12 +1,12 @@
 $(document).ready(function() {
-	$('.accordion .accordion__item .item__title').click(function(e) {
+	$('.accordion > .accordion__item > .item__title').click(function(e) {
 		var accordion = $(this).closest('.accordion'),
 				onlyOneOpen = true;
 
 		if(onlyOneOpen) {
 			var open = accordion.find('.accordion__item--is-open');
 			if(open.length && !open.is($(this).parent('.accordion__item'))) {
-				open.find('.item__content').slideToggle('fast');
+				open.find('> .item__content').slideToggle('fast');
 				open.toggleClass('accordion__item--is-open');
 			}
 		}
